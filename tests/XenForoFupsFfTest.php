4e3e72893e01b4595c2294fa5e2c6e364a8cc9d0
Peter N. Steinmetz <PeterNSteinmetz@steinmetz.org>
2022-10-18 16:34:59 -0700

    Initial unit test.

---
+++ tests/XenForoFupsFfTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace FFTest;
+
+use XenForoFUPS;
+use PHPUnit\Framework\TestCase;
+
+class XenForoFupsFfTest extends TestCase {
+    public function testBasicConstruction() {
+        $this->assertTrue(true);
+    }
+
+}
